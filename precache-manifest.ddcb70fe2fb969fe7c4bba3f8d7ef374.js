d5c7f8a592644f18fbdc5d91735aef146da0a127
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-04-26 18:23:13 +0900

    Updates

---
+++ precache-manifest.ddcb70fe2fb969fe7c4bba3f8d7ef374.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcc3297197dd8638f94bc672ec60a16a",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "f051d5a1d31cc341a7b7",
+    "url": "/RobotFriends/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "344938a9bc7aa248ab79",
+    "url": "/RobotFriends/static/css/main.c22311d4.chunk.css"
+  },
+  {
+    "revision": "f051d5a1d31cc341a7b7",
+    "url": "/RobotFriends/static/js/2.479e6b44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RobotFriends/static/js/2.479e6b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "344938a9bc7aa248ab79",
+    "url": "/RobotFriends/static/js/main.15b8ac0e.chunk.js"
+  },
+  {
+    "revision": "a3b351ce21df176961fd",
+    "url": "/RobotFriends/static/js/runtime-main.9e504aa0.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
