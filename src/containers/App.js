9bc5554c599c1e398a031994744fbf19c6fee02c
Gen Yap Feng Yuan <58755531+TheKinng96@users.noreply.github.com>
2020-04-28 16:33:03 +0900

    added redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import ErrorBoundry from '../components/ErrorBoundry';
-import Scroll from '../components/Scroll'
-// import ErrorBoundry from '../ErrorBoundry';
+import Scroll from '../components/Scroll';
+
+import { setSearchField, requestRobots } from '../action';
 // import { robots } from "../robot"; //if the file only contain one thing, use {} to get it
 
 
@@ -19,35 +21,40 @@ React life cycle method
     updating
     unmounting
 */
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+} //where to get the command or materials for the actions
+
+const mapDispatchToProps = (dispatch) =>{
+    return{
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobot: () => dispatch(requestRobots())
+    }
+} //which actions should be done
+
 class App extends Component{
-    constructor(){
-        super();
-        this.state = {
-            robots: [],
-            searchfield:''
-        }
-    }
-
+    
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({ robots: users }))
+        this.props.onRequestRobot();
     }
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
     render(){
-        const { robots, searchfield } = this.state;
+        const { searchField, onSearchChange,robots, isPending } = this.props;
         const filterRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+            return robot.name.toLowerCase().includes(searchField.toLowerCase())
         });
-        return !robots.length ? 
+        return isPending ? 
             <h1>Loading</h1> : 
             (
             <div className="tc">
                 <h1 className="f1" >RobotFriends</h1>
-                <SearchBox searchChange = {this.onSearchChange}/>
+                <SearchBox searchChange = {onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
                         <CardList robots={filterRobots} />
@@ -61,4 +68,4 @@ class App extends Component{
 // to make we only scroll inside the content, we need to use children <Scroll>
 //state is a two ways communication while props is just one way
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
